ffc0fa95d52d9e3ce3e877b0e17fe31161e60b5b
Josh R <seventytwoapp@gmail.com>
2020-05-25 10:29:53 -0500

    Initial Commit

---
+++ FirebaseAuth+SignInWithApple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseAuth+SignInWithApple
+//
+//  Created by Josh R on 5/25/20.
+//  Copyright © 2020 Josh R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
